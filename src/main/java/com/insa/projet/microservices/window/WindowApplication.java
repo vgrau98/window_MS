a022cc31afc9dc7dcade00f255d88cdab0005a32
vgrau98 <victor.grau93@gmail.com>
2021-12-24 18:08:49 +0100

    first commit

---
+++ src/main/java/com/insa/projet/microservices/window/WindowApplication.java
@@ -0,0 +1,13 @@
+package com.insa.projet.microservices.window;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insa/projet/microservices/window/WindowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insa.projet.microservices.window;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
